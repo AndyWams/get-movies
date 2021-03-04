0f6c516ea3ac9df888511f84c4bbee219b9da896
wams <andrew@lightbulbux.com>
2021-03-04 10:14:42 +0100

    added loading spinner

--- src/components/searchform.js
+++ src/components/searchform.js
@@ -2,19 +2,23 @@ import React, { useState } from "react";
 import "./search.scss";
 import axios from "axios";
 import CardList from "./cardlist";
+import Spinner from "./spinner/spinner";
 require("dotenv").config();
 function SearchForm() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   const searchMovies = (e) => {
     e.preventDefault();
+    setLoading(true);
     if (query !== "") {
       axios
         .get(
           `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_BASE_URL_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
         )
         .then((response) => {
+          setLoading(false);
           const data = response.data;
           if (data.results.length === 0) {
             setError("No Results Found!");
@@ -49,6 +53,7 @@ function SearchForm() {
           </button>
         </form>
         {error !== "" ? <p>{error}</p> : null}
+        {loading ? <Spinner /> : null}
         <div className="card-list">
           {movies
             .filter((movie) => movie.poster_path)
